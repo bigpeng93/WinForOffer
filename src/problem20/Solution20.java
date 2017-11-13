b26209c2b5787853db0fda4877d8fe86639624e1
luajdc <1099460085@qq.com>
2017-11-13 17:35:46 +0800

    WinForOffer

---
+++ src/problem20/Solution20.java
@@ -0,0 +1,36 @@
+package problem20;
+
+public class Solution20 {
+    static int strindex =0;
+    public boolean isNumberric(char[] str){
+        if(str == null){
+            return false;
+        }
+
+        boolean numeric = scanInteger(str);
+        if(strindex<str.length && str[strindex] == '.'){
+            strindex ++;
+            numeric = scanUndsignedInteger(str) || numeric;
+        }
+        if (strindex<str.length && (str[strindex]=='e'||str[strindex]=='E')){
+            strindex++;
+            numeric = numeric && scanInteger(str);
+        }
+        return numeric;
+
+    }
+    public boolean scanInteger(char[] str){
+        if(strindex<str.length && (str[strindex] == '+' || str[strindex]=='-')){
+            strindex++;
+        }
+        return scanUndsignedInteger(str);
+    }
+    public boolean scanUndsignedInteger(char[] str){
+        int before = 0;
+        while (strindex<str.length && str[strindex] >= '0' && str[strindex]<='9'){
+            before++;
+            strindex++;
+        }
+        return (before>0);
+    }
+}
